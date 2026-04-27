d8593e63a3c3280fb4f053d5c3689faa09d646b1
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 11:41:11 -0300

    feat: configuração estática
    intent: feature

--- Settings.cs
+++ Settings.cs
@@ -1,5 +1,5 @@
 namespace AppConfig;
 
 public static class Settings {
-    public const string AppName = "Observabilidade: métricas, health agregado";
+    public const string AppName = "Ledger de idempotência distribuído";
 }
