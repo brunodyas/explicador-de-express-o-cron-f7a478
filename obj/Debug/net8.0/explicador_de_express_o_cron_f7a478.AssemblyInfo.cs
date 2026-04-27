fababf130794f593eab851896ddbce2682f27edc
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 11:52:19 -0300

    chore: projeto console .NET 8
    intent: chore

--- obj/Debug/net8.0/explicador_de_express_o_cron_f7a478.AssemblyInfo.cs
+++ obj/Debug/net8.0/explicador_de_express_o_cron_f7a478.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("explicador_de_express_o_cron_f7a478")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ac2365195367927b206092456193c5316fbff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8593e63a3c3280fb4f053d5c3689faa09d646b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("explicador_de_express_o_cron_f7a478")]
 [assembly: System.Reflection.AssemblyTitleAttribute("explicador_de_express_o_cron_f7a478")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
